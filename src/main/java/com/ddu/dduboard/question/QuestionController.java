d8b527f3b482fe06607859ceb21e86459b0a56ce
ddujeong <qoddl159@nate.com>
2025-09-08 12:52:38 +0900

    DataNotFoundException 클래스 생성 전

--- src/main/java/com/ddu/dduboard/question/QuestionController.java
+++ src/main/java/com/ddu/dduboard/question/QuestionController.java
@@ -1,18 +1,23 @@
 package com.ddu.dduboard.question;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class QuestionController {
 	
-	@Autowired
-	private QuestionRepository questionRepository;
+	// @Autowired
+	// private QuestionRepository questionRepository;
 
+	@Autowired
+	private QuestionService questionService;
+	
 	@GetMapping(value = "/")
 	public String root() {
 		// url 기본 루트 url 설정 (-> 서버 start 시 리스트로 이동)
@@ -22,10 +27,19 @@ public class QuestionController {
 	@GetMapping(value = "/question/list")
 	// @ResponseBody // 리턴값의 문자열이 그대로 뷰에 찍힘
 	public String list(Model model) {
-		List<Question> questionList = questionRepository.findAll();
+		// List<Question> questionList = questionRepository.findAll();
 		// SELECT * FROM question
+		List<Question> questionList = questionService.getList();
+		
 		model.addAttribute("questionList",questionList);
 		
 		return "question_list";
+	}
+	@GetMapping(value = "/question/detail/{id}") // 파라미터 이름 없이 값만 넘어왔을때 처리
+	public String detail(Model model ,@PathVariable("id") Integer id) {
+		Question question =questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		
+		return"question_detail";
 	}
 }
--- src/main/java/com/ddu/dduboard/question/QuestionService.java
+++ src/main/java/com/ddu/dduboard/question/QuestionService.java
@@ -1,12 +1,30 @@
 package com.ddu.dduboard.question;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class QuestionService {
 	
-	@Autowired
-	private QuestionRepository questionRepository;
+	// @Autowired
+	private final QuestionRepository questionRepository;
+	// @RequiredArgsConstructor 에 의해 생성자 방식으로 주입된 questionRepository 
 
+	public List<Question> getList(){
+		return questionRepository.findAll();
+	}
+	public Question getQuestion(Integer id){
+		Optional<Question> qOptional =questionRepository.findById(id);
+		
+		if (qOptional.isPresent()) {
+			return qOptional.get();
+		} else {
+			throw new DataNotFoundException("question not found");
+		}
+	}
 }
