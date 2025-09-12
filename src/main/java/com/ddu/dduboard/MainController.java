07d53e26231286ce1f8d89b9db8c06eaabaf6b14
ddujeong <qoddl159@nate.com>
2025-09-12 17:53:04 +0900

    시간 수정

--- src/main/java/com/ddu/dduboard/MainController.java
+++ src/main/java/com/ddu/dduboard/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 	
 	// @GetMapping(value = "/") // 로컬용 root 요청 처리
-	@GetMapping(value = "/dduboard") // 오라클 클라우드용 root 요청 처리
+	@GetMapping(value = "/") // 오라클 클라우드용 root 요청 처리
 	public String root() {
 		// url 기본 루트 url 설정 (-> 서버 start 시 리스트로 이동)
 		return "redirect:/question/list";
