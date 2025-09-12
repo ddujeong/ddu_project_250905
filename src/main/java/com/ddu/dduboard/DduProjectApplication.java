e56a3438a4e2ae13c4241354d79cc180fc10f79d
ddujeong <qoddl159@nate.com>
2025-09-12 14:09:32 +0900

    어플리케이션.java 수정 추기ㅏ

--- src/main/java/com/ddu/dduboard/DduProjectApplication.java
+++ src/main/java/com/ddu/dduboard/DduProjectApplication.java
@@ -2,12 +2,20 @@ package com.ddu.dduboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DduProjectApplication {
+public class DduProjectApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(DduProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DduProjectApplication.class, args);
 	}
-
+	
 }
--- src/main/java/com/ddu/dduboard/MainController.java
+++ src/main/java/com/ddu/dduboard/MainController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-	@GetMapping(value = "/")
+	// @GetMapping(value = "/") // 로컬용 root 요청 처리
+	@GetMapping(value = "/dduboard") // 오라클 클라우드용 root 요청 처리
 	public String root() {
 		// url 기본 루트 url 설정 (-> 서버 start 시 리스트로 이동)
 		return "redirect:/question/list";
